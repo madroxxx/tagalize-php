6bb7ef46ccddebe83563bcc0375c5638e92acd2a
Jason <jj@digitaldashmedia.com>
2019-09-24 16:15:14 +0800

    new changes, added construct to test

--- src/index.php
+++ src/index.php
@@ -3,6 +3,9 @@ namespace TagalizePhp;
 
 class Index
 {
+	public function __construct(){
+		echo 'start'."\n\n";
+	}
 	public function getTagalized($string = "Kamusta")
 	{
 		return $string . '-EY';
